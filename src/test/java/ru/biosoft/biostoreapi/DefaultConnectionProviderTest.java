dc8943b0fffcd9c834c14a9f48fd6e9d61d65547
uuinnk <uuinnk@developmentontheedge.com>
2018-04-10 15:19:03 +0700

    add test

---
+++ src/test/java/ru/biosoft/biostoreapi/DefaultConnectionProviderTest.java
@@ -0,0 +1,27 @@
+package ru.biosoft.biostoreapi;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+public class DefaultConnectionProviderTest
+{
+    @Test
+    public void authorize() throws Exception
+    {
+        DefaultConnectionProvider test = new DefaultConnectionProvider("biblio.biouml.org");
+        UserPermissions authorize = test.authorize("", "", null);
+
+        assertEquals(1, authorize.getDbToPermission().size());
+        assertEquals(3, authorize.getDbToPermission().get("data/Collaboration/Demo").getPermissions());
+
+        List<String> projectList = test.getProjectList("", "");
+
+        assertEquals(1, projectList.size());
+        assertEquals("Demo", projectList.get(0));
+    }
+
+}
